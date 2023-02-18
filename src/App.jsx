34f7ec0286d62f744e373ea741c227e1dfafffd7
ninhadmit147 <59793130+ninhadmit147@users.noreply.github.com>
2023-02-18 22:37:09 +0700

    #4 commit payPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import './App.css'
+import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./page/home";
 import NotFound from "./page/notFound";
 import PrivateLayout from "./Layout/privateLayout";
 import Layout from "./Layout/layout";
-
+import PayCart from "./page/payCart/payCart";
 
 function App() {
-  return <div className="App">
-        <Routes>
-            {/* <Route path="/" 
+  return (
+    <div className="App">
+      <Routes>
+        {/* <Route path="/" 
             element={
                 <PrivateLayout>
                     <Layout/>
                 </PrivateLayout>
             }>
                 <Route index element={<Navigate to ="/"/>} /> */}
-                <Route path="/" element={ <Home /> }/>
-            {/* </Route> */}
-            {/*  */}
-            <Route path="*" element={ <NotFound /> }/>
-        </Routes>
-
-    </div>;
+        <Route path="/" element={<Home />} />
+        {/* </Route> */}
+        {/*  */}
+        <Route path="/payPage" element={<PayCart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
