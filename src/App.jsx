dc5a78895c57af20dcac340b2168dbcb1c2d2c22
ngotiendat <datmau20@gmail.com>
2023-03-03 15:07:24 +0700

    #24 - update/ui_login-register

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
-import './App.css'
+
 import { Navigate, Route, Routes } from "react-router-dom";
+import './App.css';
+import AdminLayout from './Layout/AdminLayout';
+import AdminPage from './page/Admin/home/AdminPage';
 import Home from "./page/home";
 import NotFound from "./page/notFound";
+
+import ShopCart from "./page/cart/shop_cart";
+
+import NewProduct from './page/Admin/newProduct/NewProduct';
+import NewUser from './page/Admin/newUser/NewUser';
+import Product from './page/Admin/product/Product';
+import ProductList from './page/Admin/productList/ProductList';
+import User from './page/Admin/user/User';
+import UserList from './page/Admin/userList/UserList';
 import PrivateLayout from "./Layout/privateLayout";
 import Layout from "./Layout/layout";
+import PayCart from "./page/payCart/payCart";
 import Login from './page/auth/login';
 import Register from './page/auth/register';
-
+import BLOG from "./page/blog";
 
 function App() {
     return <div className="App">
+
           <Routes>
-            <Route path="/" 
+            
+            <Route path="/"
             element={
                 <PrivateLayout>
                     <Layout/>
                 </PrivateLayout>
             }>
-            <Route index element={<Navigate to ="/"/>} />
-            <Route path="/" element={<Home />} />
+                <Route index element={<Navigate to ="/"/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog" element={<BLOG />} />
+                {/* Router Admin */}
+                <Route path='/admin' element={<AdminLayout />}>
+                    <Route index element={<AdminPage />} />
+                    <Route path='users' element={<UserList />} />
+                    <Route path='user/:userId' element={<User />} />
+                    <Route path='newUser' element={<NewUser />} />
+                    <Route path='products' element={<ProductList />} />
+                    <Route path='product/:productId' element={<Product />} />
+                    <Route path='newproduct' element={<NewProduct />} />
+                </Route>
             </Route>
 
+            <Route path="*" element={<NotFound />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<NotFound />} />
-        </Routes>
 
+        </Routes>
     </div>;
 }
 
-export default App
+export default App;
