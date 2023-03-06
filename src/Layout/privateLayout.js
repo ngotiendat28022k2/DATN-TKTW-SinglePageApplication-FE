e3e9f45b3493f36b825276b0ddd1ef17eca33516
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-03-06 22:00:57 +0700

    update private

--- src/Layout/privateLayout.js
+++ src/Layout/privateLayout.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const PrivateLayout = ({ children }) => {
-  const navigate = useNavigate();
-  const user = JSON.parse(localStorage.getItem("user"));
-  if (!user) {
-    navigate("/signin");
-  }
+//   const navigate = useNavigate();
+//   const user = JSON.parse(localStorage.getItem("user"));
+//   if (!user) {
+//     navigate("/signin");
+//   }
   return children;
 };
 
