462f6af6409df68bf48e378fb0743973b5959739
ngotiendat <datmau20@gmail.com>
2023-03-14 18:02:16 +0700

    #35 - feature/footer2

--- OneDrive/Desktop/DATN-TKTW-SinglePageApplication-FE-develop/src/Layout/ClientLayout.jsx
+++ OneDrive/Desktop/DATN-TKTW-SinglePageApplication-FE-develop/src/Layout/ClientLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/footer'
 
 const ClientLayout = () => {
   return (
@@ -8,12 +9,12 @@ const ClientLayout = () => {
             header
             </header>
 
-            <div>
+            <div className='max-w-[1280px] w-full m-auto'>
                 <Outlet/>
             </div>   
-            <footer>
-                footer
-            </footer>
+         <div>
+            <Footer/>
+         </div>
     </>
   )
 }
