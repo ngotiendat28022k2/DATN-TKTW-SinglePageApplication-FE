07f08713ce160f5a51155227baf1437edccb312a
Le Duong <duongld.0511@gmail.com>
2023-03-27 11:11:40 +0700

    #53 Fix/HeaderAdmin & Feature/Drawer Siderbar

--- src/components/AdminComponent/header/index.jsx
+++ src/components/AdminComponent/header/index.jsx
@@ -1,7 +1,16 @@
+import * as React from "react";
 import PropTypes from "prop-types";
 // @mui
-import { AppBar, Box, IconButton, Stack, Toolbar } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+// import MenuIcon from "@mui/icons-material";
 // utils
 import { bgBlur } from "../../../utils/cssStyles";
 // components
@@ -10,6 +19,7 @@ import Iconify from "../../../components/AdminComponent/iconify";
 import AccountPopover from "./AccountPopover";
 import LanguagePopover from "./LanguagePopover";
 import NotificationsPopover from "./NotificationsPopover";
+import MenuIcon from "@mui/icons-material/Menu";
 
 // ----------------------------------------------------------------------
 
@@ -17,6 +27,7 @@ const HEADER_MOBILE = 64;
 
 const StyledRoot = styled(AppBar)(({ theme }) => ({
   backgroundColor: "#8baf67",
+  position: "static",
   boxShadow: "none",
   [theme.breakpoints.up("lg")]: {
     // width: `calc(100% - ${NAV_WIDTH + 1}px)`,
@@ -26,6 +37,7 @@ const StyledRoot = styled(AppBar)(({ theme }) => ({
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   minHeight: HEADER_MOBILE,
+  position: "relative",
   [theme.breakpoints.up("lg")]: {
     minHeight: 55,
     padding: theme.spacing(0, 5),
@@ -38,38 +50,48 @@ Header.propTypes = {
   onOpenNav: PropTypes.func,
 };
 
-export default function Header({ onOpenNav }) {
+export default function Header() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
   return (
-    <StyledRoot>
-      <StyledToolbar>
-        {/* <IconButton
-          onClick={onOpenNav}
-          sx={{
-            mr: 1,
-            color: "text.primary",
-            display: { lg: "none" },
-          }}
-        >
-          <Iconify icon="eva:menu-2-fill" />
-        </IconButton> */}
-        <a href="/home">
-          <h1 sx={{ fontSize: "20px" }}>LOGO</h1>
-        </a>
-        <Box sx={{ flexGrow: 1 }} />
+    // <StyledRoot open={open}>
+    //   <StyledToolbar>
+    //     <IconButton
+    //       color="inherit"
+    //       aria-label="open drawer"
+    //       onClick={handleDrawerOpen}
+    //       edge="start"
+    //       sx={{
+    //         marginRight: 5,
+    //         ...(open && { display: "none" }),
+    //       }}
+    //     >
+    //       <MenuIcon />
+    //     </IconButton>
+    //     <a href="/home">
+    //       <h1 sx={{ fontSize: "20px" }}>LOGO</h1>
+    //     </a>
+    <div>
+      <Box sx={{ flexGrow: 1 }} />
 
-        <Stack
-          direction="row"
-          alignItems="center"
-          spacing={{
-            xs: 0.5,
-            sm: 1,
-          }}
-        >
-          <LanguagePopover />
-          <NotificationsPopover />
-          <AccountPopover />
-        </Stack>
-      </StyledToolbar>
-    </StyledRoot>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={{
+          xs: 0.5,
+          sm: 1,
+        }}
+      >
+        <LanguagePopover />
+        <NotificationsPopover />
+        <AccountPopover />
+      </Stack>
+    </div>
+
+    //   </StyledToolbar>
+    // </StyledRoot>
   );
 }
