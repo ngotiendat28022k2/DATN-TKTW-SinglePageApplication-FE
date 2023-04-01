457361a8bbcc31823e2df549c08dd190ff5283c0
trandinhphuc281 <trandinhphuc281@gmail.com>
2023-04-01 16:51:05 +0700

    #70 - frature/product-layout

--- src/slice/productsSlice.js
+++ src/slice/productsSlice.js
@@ -1,9 +1,27 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import productApi from "../api/products";
-export const fetchs = createAsyncThunk("product/fetch", async () => {
+
+export const getAllProduct = createAsyncThunk("product/getAllProduct", async () => {
   const response = await productApi.ProductList();
   return response;
 });
+
+export const getProduct = createAsyncThunk("product/:id", async (id) => {
+  console.log("id", id);
+  const respone = await productApi.ProductDetail(id);
+  return respone;
+})
+
+export const AddNewProduct = createAsyncThunk("product/AddNewProduct", async (product) => {
+  const response = await productApi.ProductAdd(product);
+  return response;
+});
+
+export const RemoveProduct = createAsyncThunk("product/:id", async (id) => {
+  console.log("id", id);
+  const respone = await productApi.RemoveProduct(id);
+  return respone;
+})
 
 const initialState = {
   value: [],
@@ -15,10 +33,22 @@ export const productSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchs.fulfilled, (state, action) => {
+    builder.addCase(getAllProduct.fulfilled, (state, action) => {
       state.value = action.payload;
       if (!state.value.length) return;
     });
+    // builder.addCase(getProduct.fulfilled, (state, action) => {
+    //   state.value = action.payload;
+    //   if (!state.value.length) return;
+    // });
+    builder.addCase(AddNewProduct.fulfilled, (state, action) => {
+      state.value = action.payload;
+      if (!state.value.length) return;
+    });
+    builder.addCase(RemoveProduct.fulfilled, (state, action) => {
+      state.value = action.payload;
+      if (!state.value.length) return;
+    })
   },
 });
 
