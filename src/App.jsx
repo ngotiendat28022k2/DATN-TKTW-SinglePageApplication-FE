52de3c4166b59b0006af753ec897389dbfa0cc62
datdevelop2802 <79825535+ngotiendat28022k2@users.noreply.github.com>
2023-04-08 18:42:51 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,34 @@ import AccountLayout from "./Layout/LayoutAccount";
 import DetailProduct from "./page/Detail";
 import CheckoutCart from "./page/checkout-cart";
 //
-import PagePay from "./page/pay/index"
+import PagePay from "./page/pay/index";
 import PageSearch from "./page/pageSearch";
 //
-import AdminPage from './page/Admin/home/AdminPage'
-import ProductList from './page/Admin/ProductList'
-const UserList = React.lazy(() => import('./page/Admin/userList/UserList'));
-const ProfileAdmin = React.lazy(() => import('./page/Admin/Profile/Profile.admin'));
-// 
-const ProfileAddress = React.lazy(() => import('./page/profile/Profile-Address'));
-const ProfileInfo = React.lazy(() => import('./page/profile/Profile-Info'));
-const ProfileMyProduct = React.lazy(() => import('./page/profile/Profile-MyProduct'));
-const ProfileNotification = React.lazy(() => import('./page/profile/Profile-Notification'));
-const ProfileVoucher = React.lazy(() => import('./page/profile/Profile-Voucher'));
-const HomePage = React.lazy(() => import('./page/home'));
-const Blog = React.lazy(() => import('./page/blog'));
-const Login = React.lazy(() => import('./page/auth/login'));
-const Register = React.lazy(() => import('./page/auth/register'));
-const Search = React.lazy(() => import('./page/pageSearch'));
+import AdminPage from "./page/Admin/home/AdminPage";
+import ProductList from "./page/Admin/ProductList";
+const UserList = React.lazy(() => import("./page/Admin/userList/UserList"));
+const ProfileAdmin = React.lazy(() =>
+  import("./page/Admin/Profile/Profile.admin")
+);
+//
+const ProfileAddress = React.lazy(() =>
+  import("./page/profile/Profile-Address")
+);
+const ProfileInfo = React.lazy(() => import("./page/profile/Profile-Info"));
+const ProfileMyProduct = React.lazy(() =>
+  import("./page/profile/Profile-MyProduct")
+);
+const ProfileNotification = React.lazy(() =>
+  import("./page/profile/Profile-Notification")
+);
+const ProfileVoucher = React.lazy(() =>
+  import("./page/profile/Profile-Voucher")
+);
+const HomePage = React.lazy(() => import("./page/home"));
+const Blog = React.lazy(() => import("./page/blog"));
+const Login = React.lazy(() => import("./page/auth/login"));
+const Register = React.lazy(() => import("./page/auth/register"));
+const Search = React.lazy(() => import("./page/pageSearch"));
 
 function App() {
   return (
@@ -42,8 +52,6 @@ function App() {
               <Route index element={<Navigate to="home" />} />
               <Route path="home" element={<HomePage />} />
               <Route path="blog" element={<Blog />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="wishlist" element={<WishList />} />
               <Route path="/detail/:id" element={<DetailProduct />} />
               <Route path="/checkout" element={<CheckoutCart />} />
               <Route path="/pay" element={<PagePay />} />
@@ -58,29 +66,27 @@ function App() {
                 />
               </Route>
             </Route>
-            {/* <Route path="/orderSearch" element={<OrderSearch />} /> */}
+            {/* <Route path="/paycart" element={<PayCart />} /> */}
+            {/* Router Admin */}
             <Route
-              path={Slug("quyển sách này thật tuyệt")}
-              element={<OrderSearch />}
-            />
-             {/* <Route path="/paycart" element={<PayCart />} /> */}
-                    </Route>
+              path="admin/"
+              element={
+                <PrivateLayout>
+                  <AdminLayout />
+                </PrivateLayout>
+              }
+            >
+              <Route index element={<AdminPage />} />
+              <Route path="users" element={<UserList />} />
+              {/* <Route path="publishs" element={<PublishingList />} /> */}
+              {/* <Route path="categories" element={<CategoryList />} /> */}
+              <Route path="products" element={<ProductList />} />
+              <Route path="profile" element={<ProfileAdmin />} />
+            </Route>
 
-                    {/* Router Admin */}
-                    <Route path='admin/' element={<PrivateLayout><AdminLayout /></PrivateLayout>}>
-                        <Route index element={<AdminPage />} />
-                        <Route path='users' element={<UserList />} />
-                        <Route path='products' element={<ProductList />} />
-                        <Route path='profile' element={<ProfileAdmin />} />
-                        Route path="publishs" element={<PublishingList />} />
-                        <Route path="categories" element={<CategoryList />} />
-                        <Route path="products" element={<ProductList />} />
-                        <Route path="profile" element={<ProfileAdmin />} />
-                    </Route>
-                    
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </React.Suspense>
